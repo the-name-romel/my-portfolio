07f4c6c9972c9c7899fd04a1eac0f9b752689e8a
the-name-romel <49654024+the-name-romel@users.noreply.github.com>
2022-12-01 05:48:40 +0000

    add hover glass background on project item

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export const Project = ({ image, alt, title }) => {
   return (
-    <div className="relative max-h-[342px] max-w-[387px] h-full w-full">
+    <div className="relative max-h-[342px] max-w-[387px] h-full w-full rounded-[10px] overflow-hidden duration-300">
       <img src={image} alt={alt} className="object-cover w-full h-full" />
-      <div className="absolute flex flex-col top-0 left-0 h-full w-full bg-opacity-0 group hover:bg-opacity-70 bg-primary items-center justify-center">
-        <h1 className="text-white">{title}</h1>
+      <div className="absolute flex flex-col top-0 left-0 h-full w-full bg-opacity-0 group hover:bg-opacity-70 bg-primary items-center justify-center duration-300 hover:backdrop-blur-[5px] ease-in-out">
+        <h1 className="text-white hidden group-hover:block">{title}</h1>
         <a href=""></a>
       </div>
     </div>
