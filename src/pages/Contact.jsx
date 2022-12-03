2103ab1eeefdaa335aff127e7a2107f8f9e2aaf4
the-name-romel <49654024+the-name-romel@users.noreply.github.com>
2022-12-03 13:24:03 +0000

    finishing contact section on mobile view

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,21 +1,29 @@
-import React from 'react'
+import React from "react";
 
 function Contact() {
   return (
-    <div>
-      <div>
+    <div className="w-full">
+      <div className="w-full py-[50px] px-[30px]">
         <div className="mx-auto text-center">
           <h1 className="text-primary text-[32px]">CONTACT</h1>
           <p>Get in touch</p>
         </div>
 
-        <form action="">
-            <input type="text" placeholder='Name' />
+        <form action="" className="w-full space-y-5 text-white">
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email" />
+          <input type="text" placeholder="Subject" />
+          <textarea name="" id="" rows="10" placeholder="I love chicken nuggeeeets..." className="w-full rounded-[10px] bg-primary bg-opacity-50" />
+          <button
+            type="submit"
+            className="border bg-primary rounded-[8px] text-white text-[18px] px-[28px] py-[16px] mt-[30px] hover:bg-opacity-80"
+          >
+            Send message
+          </button>
         </form>
       </div>
     </div>
   );
 }
 
-export default Contact
-
+export default Contact;
